01f0490638def3e045729929a2cfc624a04dc67e
김지훈 <solev0025@gmail.com>
2026-05-18 17:38:22 +0900

    플레이어 이동 관련 구현
    플레이어 wasd 로 이동
    마우스로 시야 이동(90도 각도 제한)
    1인칭 모델 부여 및 애니메이션 할당(추후 모델이 변경 가능성있음)
    플레이어 질주 기능
    임시로 Reload InputAction에 추가하고 애니메이션 + Log만 띄우는 형태로 틀만 잡아놓음.

--- Assets/Scripts/Characters/PlayerMoves.cs
+++ Assets/Scripts/Characters/PlayerMoves.cs
@@ -4,12 +4,25 @@ using UnityEngine.InputSystem;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 using Quaternion = UnityEngine.Quaternion;
+using InfimaGames.LowPolyShooterPack;
 
 public class PlayerMoves : MonoBehaviour
 {
      [SerializeField]private float speed = 5f;
      [SerializeField]private float mouseSensitivity = 1f;
      [SerializeField]private Camera playerCamera;
+
+     [SerializeField]private float fireRange = 100f;
+     [SerializeField]private float fireInterval = 0.1f;
+     
+     [SerializeField]private LayerMask shootableLayer;
+     [SerializeField]private ParticleSystem muzzleFlash;
+     [SerializeField]private Animator weaponAnimator;
+
+     private bool isFiring = false;
+     private bool isSprinting = false;
+     private float nextFireTime;//발사 간격
+
      
     
      private CharacterController controller;
@@ -18,25 +31,33 @@ public class PlayerMoves : MonoBehaviour
 
      private Vector3 velocity;
     private float xRotation;
+
+    private Animator animator;
      
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        if (muzzleFlash == null)
+            muzzleFlash = GetComponentInChildren<ParticleSystem>(true);
+
+        if (weaponAnimator == null)
+            weaponAnimator = GetComponentInChildren<Animator>(true);
+
+        animator = weaponAnimator;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-    }
-    
-         
-    void Start()
-    {
-        
+        if (muzzleFlash != null)
+            muzzleFlash.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
          HandleLook();
+         HandleSprint();
          HandleMove();
+         HandleFire();  
+         HandleReload();
     }
     
     public void OnMove(InputAction.CallbackContext context)
@@ -51,7 +72,12 @@ public class PlayerMoves : MonoBehaviour
      {
         Vector3 moveDir = transform.right * moveInput.x + 
                         transform.forward * moveInput.y;
-        controller.Move(moveDir * speed * Time.deltaTime);
+        float currentSpeed;
+        if(isSprinting)
+            currentSpeed = speed * 2f;
+        else
+            currentSpeed = speed;
+        controller.Move(moveDir * currentSpeed * Time.deltaTime);
         
         /// 3. 지면 감지 후 중력 처리
         if (controller.isGrounded && velocity.y < 0)
@@ -72,6 +98,73 @@ public class PlayerMoves : MonoBehaviour
 
          transform.Rotate(Vector3.up * mouseX);
      }
+     public void OnFire(InputAction.CallbackContext context)
+     {
+         if (context.started)
+             isFiring = true;
+         else if (context.canceled)
+             isFiring = false;
+
+         if (isFiring)
+             HandleFire();
+     }
+        private void HandleFire()
+        {
+            if(!isFiring)
+                return;
+
+            if (Time.time < nextFireTime)
+                return;
+
+            nextFireTime = Time.time + fireInterval;
+
+            if(animator != null)
+                animator.SetTrigger("Fire");
+
+            if (muzzleFlash != null)
+            {
+                muzzleFlash.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                muzzleFlash.Play();
+            }
+
+            Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            Debug.DrawRay(ray.origin, ray.direction * fireRange, Color.red, 1f);
+            if (Physics.Raycast(ray, out RaycastHit hit, fireRange, shootableLayer))
+            {
+                Debug.Log($"Hit: {hit.collider.name}");
+
+            }
+        }
+
+        //sprint 메서드
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            if (context.started)
+                isSprinting = true;  
+            else if (context.canceled)
+                isSprinting = false;  
+        }
+        private void HandleSprint()
+        {
+            isSprinting = Keyboard.current != null && Keyboard.current.leftShiftKey.isPressed;
+        }
+        public void OnReload(InputAction.CallbackContext context)
+        {
+            if (context.started)
+                HandleReload();
+        }
+        private void HandleReload()
+        {
+            //재장전 애니메이션 트리거
+            if (Keyboard.current != null && Keyboard.current.rKey.wasPressedThisFrame)
+            {
+                if (animator != null)
+                    animator.SetTrigger("Reload");
+                Debug.Log("Reloading...");
+            }
+        }
     
-      
+
+ 
+        
 }
